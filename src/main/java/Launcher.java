eab693932f76d791cc5b8a93c2ba761228e3c61c
Conor Hughes <conorhughes93@gmail.com>
2016-03-29 13:01:44 +0100

    Setup Project

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+/**
+ * Launcher
+ *
+ * @author Conor Hughes - hello@conorhughes.me
+ * @version 1.0
+ * @since 29/03/2016
+ */
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
